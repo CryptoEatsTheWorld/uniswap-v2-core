f8f934e3789069d5b2d0854b4918da0ed637a3d4
Christopher Owens <69774383+CryptoEatsTheWorld@users.noreply.github.com>
2020-11-18 10:04:19 -0500

    Added safemath import back, but from another URL

--- contracts/UniswapV2ERC20.sol
+++ contracts/UniswapV2ERC20.sol
@@ -1,7 +1,7 @@
 pragma solidity =0.5.16;
 
 import './interfaces/IUniswapV2ERC20.sol';
-//import './libraries/SafeMath.sol';
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol';
 
 contract UniswapV2ERC20 is IUniswapV2ERC20 {
     using SafeMath for uint;
